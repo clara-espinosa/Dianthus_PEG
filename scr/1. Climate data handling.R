ad4ac981c5947a2ab5fd9a7a19db57b8f770664b
clara-espinosa <espinosaclara@uniovi.es>
2024-11-26 10:35:05 +0100

    Script 1 climate data cleaning and commenting

--- scr/1. Climate data handling.R
+++ scr/1. Climate data handling.R
@@ -5,66 +5,33 @@ Sys.setlocale("LC_TIME", "English")
 write.csv(bioclim, "results/bioclim.csv")
 #### iButtons ####
 # indices calculation (14 plots for dianthus subpopulations from iButtons loggers)
-read.csv("data/temperature_ibuttons.csv", sep =";") %>%
-  mutate(Time = strptime(as.character(Time), "%d/%m/%Y %H:%M"))%>% 
+read.csv("data/temperature_ibuttons.csv", sep =";") %>%  # read raw data
+  mutate(Time = strptime(as.character(Time), "%d/%m/%Y %H:%M"))%>% # specify time formar
   mutate(Time = as.POSIXct(Time, tz = "UTC")) %>%
-  merge(read.csv("data/Dianthus_header.csv", sep =",")) %>%
+  merge(read.csv("data/Dianthus_header.csv", sep =",")) %>% 
   group_by(Site,ID, Day = lubridate::floor_date(Time, "day")) %>% #
   summarise(T = mean(Temperature), X = max(Temperature), N = min(Temperature), n = length(Time)) %>% # Daily mean, max, min
-  mutate(Snow = ifelse(X < 0.5 & N > -0.5, 1, 0)) %>% # Day is snow day or not
-  mutate(FreezeThaw = ifelse(X > 0.5 & N < -0.5, 1, 0)) %>% # Day with freeze-thaw cycles
+  mutate(Snow = ifelse(X < 0.5 & N > -0.5, 1, 0)) %>% # to calculate if a day is under snow or not
   mutate(FDD = ifelse(T < 0, T, 0)) %>% # Freezing degrees per day
   mutate(GDD = ifelse(T >= 5, T, 0)) %>% # Growing degrees day per month https://link.springer.com/article/10.1007/s00035-021-00250-1
   group_by(Site, ID, Month = lubridate::floor_date(Day, "month")) %>% #
   summarise(T = mean(T), X = mean(X), N = mean(N), # Daily mean, max, min
             Snow = sum(Snow), # Snow days per month
-            FreezeThaw = sum(FreezeThaw), # Freeze-thaw days per month
             FDD = sum(FDD), # FDD per month
             GDD = sum(GDD)) %>% # GDD per month
   group_by(Site, ID) %>% #
   summarise(bio1 = mean(T), # Annual Mean Temperature
             bio2 = mean(X - N), # Mean Diurnal Range (Mean of monthly (max temp - min temp))
             bio7 = max(X) - min(N), # Temperature Annual Range (BIO5-BIO6)
-            Snw = sum(Snow),
+            Snw = sum(Snow),# snow per year
             FDD = abs(sum(FDD)), # FDD per year
             GDD = sum(GDD)) -> # GDD per year
   dianthus_bioclim_ibuttons 
 
 dianthus_bioclim_ibuttons %>% write.csv("results/dianthus_bioclim_ibuttons.csv", row.names = FALSE)
 
-# visualization data ibuttons loggers from 14 subpopulations 
-x11()
-read.csv("data/temperature_ibuttons.csv", sep =";") %>%
-  mutate(Time = strptime(as.character(Time), "%d/%m/%Y %H:%M"))%>% #specify format of TIME variable
-  mutate(Time = as.POSIXct(Time, tz = "UTC")) %>%
-  merge(read.csv("data/dianthus_header.csv", sep= ",")) %>%
-  mutate(Time = as.POSIXct(Time, tz = "UTC")) %>% 
-  ggplot(aes(Time, Temperature, color = ID)) + 
-  facet_wrap(ID ~ Site, nrow = 5) + 
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  geom_line() +
-  xlab("Time (4-h recording inverval)") +
-  ylab("Temperature (ºC)") +
-  ggthemes::theme_tufte() +
-  theme(strip.background = element_rect(colour = "grey96", fill = "grey96"),
-        legend.position = "none", 
-        legend.direction = "vertical",
-        legend.title = element_blank(),
-        legend.text = element_text(size = 16, face = "italic"), 
-        panel.background = element_rect(color = "grey96", fill = "grey96"),
-        strip.text = element_text(size = 12),
-        axis.title = element_text(size = 12),
-        axis.text.x = element_text(size = 8, color = "black"),
-        axis.text.y = element_text(size = 11, color = "black")) -> ibuttons_clima;ibuttons_clima
-
-
-### Save figure
-
-ggsave(ibuttons_clima, file = "results/ibuttons_clima(2).png", 
-       path = NULL, scale = 1, width = 182, height = 182, units = "mm", dpi = 600)
-
 #### Microlog data ####
-# indices calculation FILTERED with data ibuttons!##
+# indices calculation FILTERED to match  ibuttons data
 read.csv("data/wp_villa_2020_2024.csv", sep =",") %>%
   mutate(Time = strptime(as.character(Time), "%d/%m/%Y %H:%M"))%>% 
   mutate(Time = as.POSIXct(Time, tz = "UTC")) %>%
@@ -79,32 +46,28 @@ read.csv("data/wp_villa_2020_2024.csv", sep =",") %>%
   filter(!Year %in% 2024)%>% # exclude 2024 data
   filter (! (Year %in% 2021 & Month < 7))%>% # remove missing june days of 2021
   filter (! (Year %in% 2022 & Month > 5))%>% # remove missing july days
-  #filter(! Month %in% 6) %>% # Remove missing June days
   filter(! (Month %in% 5 & Day > 29)) %>% # Remove missing May days
   filter(! (Month %in% 7 & Day < 12)) %>% # Remove missing July days 
-  #merge(read.csv("data/Dianthus_header.csv", sep =";")) %>%
   group_by(Site, Day = lubridate::floor_date(Time, "day")) %>%
   summarise(T = mean(Temperature), X = max(Temperature), N = min(Temperature), n = length(Time)) %>% # Daily mean, max, min
   mutate(Snow = ifelse(X < 0.5 & N > -0.5, 1, 0)) %>% # Day is snow day or not
-  mutate(FreezeThaw = ifelse(X > 0.5 & N < -0.5, 1, 0)) %>% # Day with freeze-thaw cycles
   mutate(FDD = ifelse(T < 0, T, 0)) %>% # Freezing degrees per day
   mutate(GDD = ifelse(T >= 5, T, 0)) %>% # Growing degrees day per month https://link.springer.com/article/10.1007/s00035-021-00250-1
   group_by(Site, Month = lubridate::floor_date(Day, "month")) %>%
   summarise(T = mean(T), X = mean(X), N = mean(N), # Daily mean, max, min
             Snow = sum(Snow), # Snow days per month
-            FreezeThaw = sum(FreezeThaw), # Freeze-thaw days per month
             FDD = sum(FDD), # FDD per month
             GDD = sum(GDD)) %>% # GDD per month
   group_by(Site) %>%
   summarise(bio1 = mean(T), # Annual Mean Temperature
             bio2 = mean(X - N), # Mean Diurnal Range (Mean of monthly (max temp - min temp))
             bio7 = max(X) - min(N), # Temperature Annual Range (BIO5-BIO6)
-            Snw = sum(Snow),
+            Snw = sum(Snow), # snow per year
             FDD = abs(sum(FDD)), # FDD per year
-            GDD = sum(GDD))%>%
-  mutate(ID= c("D00", "A00", "C00"))%>% ##"B00", 
+            GDD = sum(GDD))%>%# GDD per year
+  mutate(ID= c("D00", "A00", "C00"))%>% # keep codes to join with iButtons data 
   mutate(Site = as.factor(Site)) %>%
-  rename(Site= Site) -> # GDD per year
+  rename(Site= Site) -> 
   dianthus_bioclim_microlog
 
 dianthus_bioclim_microlog %>% write.csv("results/dianthus_bioclim_microlog_filtered.csv", row.names = FALSE)
@@ -115,7 +78,9 @@ dianthus_bioclim_ibuttons%>%
   rbind(dianthus_bioclim_microlog)-> bioclim
 write.csv(bioclim, "results/bioclim.csv")
 
-# Field water potential data to investigate reviewer idea ####
+bioclim
+
+# Field water potential data to investigate how long might take to trigger germination in he field####
 library(dplyr)
 read.csv("data/wp_villa_2020_2024.csv", sep =",") %>%
   mutate(Time = strptime(as.character(Time), "%d/%m/%Y %H:%M"))%>% 
@@ -126,12 +91,12 @@ read.csv("data/wp_villa_2020_2024.csv", sep =",") %>%
   mutate(Hour = lubridate::hour(Time)) %>% 
   mutate(WP = rowMeans((cbind(wp1, wp2)))) %>% # mean value between WP sensors
   filter((Month < 10 & Month > 6))%>% # change month 6 or 7 depending if we focus on july or august
-  group_by(Site, Micro, Year, ID, Hour = lubridate::floor_date(Time, "hour")) %>%
+  group_by(Site, Micro, Year, ID, Hour = lubridate::floor_date(Time, "hour")) %>% # group data by hour
   summarise(WP = mean (WP), n = length(Time)) %>% # mean WP value x day
   mutate(minBWP = ifelse(WP<=3.5, 1, 0))%>% # WP values in bar 3.5 bars = -0.35 Mpa
   filter((minBWP>0))%>%
   group_by(Site, ID, Year)%>%
-  summarise(minBWP_date = first(Hour))-> minBWP_date
+  summarise(minBWP_date = first(Hour))-> minBWP_date # dates to when the  minimum germination base water potential is reached
 
 read.csv("data/wp_villa_2020_2024.csv", sep =",") %>%
   mutate(Time = strptime(as.character(Time), "%d/%m/%Y %H:%M"))%>% 
@@ -147,9 +112,9 @@ read.csv("data/wp_villa_2020_2024.csv", sep =",") %>%
   mutate(maxBWP = ifelse(WP<=5.5, 1, 0))%>%# WP values in bar 5.5 bars = -0.55 Mpa
   filter((maxBWP>0))%>%
   group_by(Site, ID, Year)%>%
-  summarise(maxBWP_date = first(Hour))->maxBWP_date
+  summarise(maxBWP_date = first(Hour))->maxBWP_date # dates to when the maximum germination base water potential is reached
 
 minBWP_date%>%
   merge(maxBWP_date, by=c("Site", "ID", "Year"))%>%
-  mutate (time_diff = difftime(minBWP_date , maxBWP_date, units = "hour")) %>% 
+  mutate (time_diff = difftime(minBWP_date , maxBWP_date, units = "hour")) %>% # differences in hours between minBWP and maxBWP 
   write.csv("results/field_WP_july.csv")

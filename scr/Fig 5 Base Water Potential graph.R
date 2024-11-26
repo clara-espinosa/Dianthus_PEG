5f9d6850e5e28c04746f4aa11ce5a3178fb5dbc8
clara-espinosa <espinosaclara@uniovi.es>
2024-11-26 11:19:22 +0100

    figure 5 graph and script updated

--- scr/Fig 5 Base Water Potential graph.R
+++ scr/Fig 5 Base Water Potential graph.R
@@ -15,22 +15,25 @@ bWP_summary %>%
   mutate(storage_treatment = recode (storage_treatment, "Fresh_seeds" = "Fresh", "After_ripened" = "After ripened"))%>%
   mutate(storage_treatment = fct_relevel(storage_treatment, "Fresh", "After ripened" ))%>%
   ggplot (aes(x=GDD, y= psib50, fill= storage_treatment), color = "black") + #, color = site
-  geom_point(size= 4, shape=21)+
+  geom_point(size= 3, shape=21)+
   geom_smooth(method = "lm", se=FALSE, level = 0.9, color = "black")+
   scale_fill_manual (name= "Storage treatment", values = c("forestgreen", "gold") ) +
   scale_color_manual (name= "Storage treatment", values = c("forestgreen", "gold") ) +
-  geom_text(data= regrpvalues, aes(y= 0.1, x= 1550,  label=lbl), color= "black", size = 5)+
+  geom_text(data= regrpvalues, aes(y= 0.1, x= 1550,  label=lbl), color= "black", size = 4)+
   facet_wrap(~storage_treatment)+
-  ggthemes::theme_tufte(base_size = 16) + 
+  theme_classic(base_size = 12) +
+  #ggthemes::theme_tufte(base_size = 16) + 
   geom_hline(yintercept=0, linetype ="dashed", linewidth =1, colour = "black")+
   theme (text = element_text(family = "sans"),
          panel.background = element_rect(color = "black", fill = NULL), #hjust = 0.5,
-         plot.title = element_text ( size = 18), #
-         strip.text = element_text (size = 16),
+         plot.title = element_text ( size = 14), #
+         strip.text = element_text (size = 12),
          strip.background =element_rect(fill="white"),
-         axis.title.y = element_text (size=14), 
-         axis.title.x = element_text (size=14), 
-         legend.title = element_text(size = 14),
-         legend.text = element_text (size =14), 
+         axis.title = element_text (size=10),  
+         legend.title = element_text(size = 12),
+         legend.text = element_text (size =10), 
          legend.position = "none")+
   labs(y = expression(paste(Psi,"b (MPa)")), x= "Growing degree days (ºC)") -> Fig5; Fig5
+
+ggsave(filename = "Figure 5.png", plot =Fig5, path = "results/figures", 
+       scale =1, width = 180, height = 100,units = "mm",device = "png", dpi = 600)
